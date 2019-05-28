0500c6748d675e147b549e5115ab40eab0efa10e
MagicLegend <jort@elska.nl>
2019-05-28 15:59:02 +0200

    + Added environment variabele test

--- src/main/java/com/magiclegend/herokudemo/TestController.java
+++ src/main/java/com/magiclegend/herokudemo/TestController.java
@@ -1,5 +1,6 @@
 package com.magiclegend.herokudemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RestController
 public class TestController {
+    @Value("${security.oauth2.client.client-id:client-id}")
+    private String clientId;
+
     @GetMapping("/test")
     String test(@RequestParam String input) {
         return "Hi " + input;
+    }
+
+    @GetMapping("/variable")
+    String variable() {
+        return "clientId: " + clientId;
     }
 }
